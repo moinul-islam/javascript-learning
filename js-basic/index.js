f879241b2708cc387f2f57a64119476def7584d6
Moin <usermoin@gmail.com>
2021-01-13 12:01:47 +0600

    Data type Learning

--- js-basic/index.js
+++ js-basic/index.js
@@ -1,1 +1,27 @@
-prompt('what is your name?');
+// Data type: string, number, boolean, undefiend, null
+
+// string: data type 1
+var fristName = 'Moinul';
+console.log(fristName);
+
+// number: data type 2
+var age = 23;
+console.log(age);
+
+// boolean: data type 3
+var isMarrid = false;
+console.log('moin is marrid? = ' + isMarrid);
+
+// undefiend: data type 4
+var job;
+console.log(job);
+
+job = 'trainer';
+job = 'a Programmer'
+console.log(job);
+
+// null: data type 5
+// no example!
+
+// data type nameing: $, _, letter, number
+// dont use reserved keyword: symboles, delete, function, if, elase
